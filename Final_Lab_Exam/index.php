bc85ec9955ac6cfa26eb9a3e411af37867700550
sadiatonny <72751160+sadiatonny@users.noreply.github.com>
2020-12-20 11:42:53 +0600

    commit at 11:35 am

---
+++ Final_Lab_Exam/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Log In</title>
+</head>
+<body>
+    
+    <center>
+        
+       
+        
+       
+            
+            <fieldset style="width:300px">
+            
+            <legend><h1>Log In</h1></legend>
+            
+            <form>
+                
+                User Id <br>
+                <input type="text" name="userId"><br><br>
+                Password <br>
+                <input type="text" name="password"><br><br>
+                <input type="checkbox" name="remember" value="Remember "> Remember Me
+                <hr>
+                <input type="submit" name="submit" value="Login"> <a href="view/registration.php">Register</a>
+                
+            </form>
+            
+        </fieldset>
+            
+            
+        
+        
+    </center>
+    
+</body>
+</html>
