9b9fd4b61f4e2c1e6d87af823f33b168cbf768a8
sadiatonny <72751160+sadiatonny@users.noreply.github.com>
2020-12-20 11:50:28 +0600

    commit at 11:35am

---
+++ Final_Lab_Exam/view/registration.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<head>
+    
+    <title>Registration</title>
+</head>
+<body>
+     <center>
+        
+        <fieldset style="width:700px;">
+            
+            <legend><h1>Registration</h1></legend>
+            
+            <form>
+                
+                Id <br>
+                <input type="text" name="id"><br>
+                Password <br>
+                <input type="password" name="id"><br>
+                Confirm password <br>
+                <input type="password" name="id"><br>
+                Name <br>
+                <input type="text" name="id"><br>
+                Email <br>
+                <input type="email" name="id"><br>
+                
+                User Type <br>
+                
+                <select name="bloodGroup" >
+				    <option value="User">User</option>
+				    
+				
+			    </select>
+               
+               <hr>
+               
+            <input type="submit" name="submit" value="Register"> <a href="index.php">Login</a>
+                
+            </form>
+            
+        </fieldset>
+        
+    </center>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+</html>
